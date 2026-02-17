17484e86063bacebb11baefb5b3250421ae2845c
Michael <michaelgizachew38@gmail.com>
2026-02-18 01:54:40 +0300

    Refactor character case counting logic

--- Word.py
+++ Word.py
@@ -1,10 +1,9 @@
 s = input()
-low_case = "abcdefghijklmnopqrstuvwxyz"
 up = 0
 low = 0
 n = len(s)
 for i in range(n):
-    if s[i] in low_case:
+    if s[i].islower():
         low = low + 1
     else:
         up = up + 1
