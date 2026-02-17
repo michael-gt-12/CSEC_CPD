460a6a7c48f2ca866594bf4870c7eb9c5d7a3cb8
Michael <michaelgizachew38@gmail.com>
2026-02-17 03:44:59 +0300

    Simplify solution logic for counting views
    Refactor input handling and counting logic for views.

--- Team.py
+++ Team.py
@@ -1,25 +1,9 @@
 n = int(input())
-sol = 0
-a = [None]*n
-for i in range(n):
-    a[i] = input()
-
-for j in range(n):
-    b,c,d = a[j].split()
-    if b == "1":
-        if c == "1":
-            sol = sol + 1
-        else:
-            if d == "1":
-                sol = sol + 1
-            else:
-                sol = sol
-    else:
-        if c == "1":
-            if d == "1":
-                sol = sol + 1
-            else:
-                sol = sol
-        else:
-            sol = sol
-print(sol)
+views = []
+for _ in range(n):
+    views.append(list(map(int,input().split())))
+count = 0
+for question in views:
+    if question.count(1) >= 2:
+        count += 1
+print(count)
