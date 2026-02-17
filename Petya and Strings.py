393699219773f8f5cdf1d3693caf7b4ceb4d2f0f
Michael <michaelgizachew38@gmail.com>
2026-02-18 01:49:04 +0300

    Refactor string comparison to use uppercase inputs
    Refactored string comparison to use uppercase inputs and removed unnecessary capitalization lists.

--- Petya and Strings.py
+++ Petya and Strings.py
@@ -1,21 +1,16 @@
-str1 = input()
-str2 = input()
+str1 = input().upper()
+str2 = input().upper()
 n = len(str1)
 count = 0
-strr1 = []
-strr2 = []
-for i in range(n):
-    strr1.append(str1[i].capitalize())
-    strr2.append(str2[i].capitalize())
 
 for i in range(n):
-    if strr1[i] == strr2[i]:
+    if str1[i] == str2[i]:
         count = 0
     else:
-        if strr1[i] < strr2[i]:
+        if str1[i] < str2[i]:
             count = -1
             break
-        elif strr2[i] < strr1[i]:
+        elif str2[i] < str1[i]:
             count = 1
             break
 print(count)
